2554b69173613eb9cb9d6eff14d5fdbd52857ec2
krasnovss91 <krasnovstanislav91@gmail.com>
2022-02-04 12:58:18 +0400

    fragments_1

---
+++ app/src/main/java/com/example/myapplication80/ContentFragment2.kt
@@ -0,0 +1,6 @@
+package com.example.myapplication80
+
+import androidx.fragment.app.Fragment
+
+class ContentFragment2: Fragment() {
+}
--- app/src/main/java/com/example/myapplication80/MainActivity.kt
+++ app/src/main/java/com/example/myapplication80/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+            //при нажатии на кнопки открывать 1 и 2 фрагменты соответственно
         val button1 = findViewById<AppCompatButton>(R.id.button1)
         val button2 = findViewById<AppCompatButton>(R.id.button2)
 
